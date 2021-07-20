ca3b6b9c994fe0c1b97e6fc7e197b404157d82bc
Edward Kerstein <ekerstein@gmail.com>
2021-07-20 15:07:11 -0400

    Revert "debug"
    This reverts commit a8271e2173f1171444381fcd5f37831fde4792cd.

--- config/conf/prod/settings.py
+++ config/conf/prod/settings.py
@@ -9,7 +9,7 @@ ALLOWED_HOSTS = ['.egograph.com'] # A list of strings representing the host/doma
 INTERNAL_IPS = type(str('c'), (), {'__contains__': lambda *a: True})() if DEBUG else [] # Allows debug variable in templates, accepts all IP addresses
 
 # SECURITY
-#SESSION_COOKIE_SECURE = True # If True, cookie is only sent with HTTPS
-#CSRF_COOKIE_SECURE = True # If True, cookie is only sent with HTTPS
-#SECURE_SSL_REDIRECT = True # If True, SecurityMiddleware redirects all non-HTTPS requests to HTTPS
-#X_FRAME_OPTIONS = 'DENY' # Default 'SAMEORIGIN', unless your site serves parts of itself in a frame, should be 'DENY'.
+SESSION_COOKIE_SECURE = True # If True, cookie is only sent with HTTPS
+CSRF_COOKIE_SECURE = True # If True, cookie is only sent with HTTPS
+SECURE_SSL_REDIRECT = True # If True, SecurityMiddleware redirects all non-HTTPS requests to HTTPS
+X_FRAME_OPTIONS = 'DENY' # Default 'SAMEORIGIN', unless your site serves parts of itself in a frame, should be 'DENY'.
